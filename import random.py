8ab33729498599f2e12ad2bf6473e4110f189d7b
Skye Jacobson <skyejacobson@users.noreply.github.com>
2023-03-03 19:22:34 -0800

    Create randINTtable.py
    Commit origin file. Feel free to leave feedback

---
+++ import random.py
@@ -0,0 +1,19 @@
+import random
+import pandas as pd
+
+# set up the table dimensions
+num_rows = '''number of rows'''
+num_cols = '''number of columns'''
+
+# calculate the average value
+avg_val = '''Your average value here'''
+
+# calculate the range of random values based on the average value
+rand_range = 2 * (avg_val - '''the whole number value of your average value''')
+
+# create a list of random numbers with 2 decimal places
+rand_list = [round(random.uniform('''range''', '''range''' + rand_range), 2) for i in range(num_rows)]
+
+# create a pandas DataFrame from the data and print it
+df = pd.DataFrame(rand_list, columns=['Column 1'])
+print(df)
